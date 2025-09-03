cb35f9fab6b0e647576f91069cdfcdcb44e161f0
Ian Valeta <valeta.iac@gmail.com>
2025-09-03 12:36:53 -0400

    initial version

---
+++ buttons.js
@@ -0,0 +1,171 @@
+export const buttonHTML = `
+<!-- 9 Button -->
+<button 
+  class="calc-button input"
+  data-name="nine"
+  data-value="9"
+  style="grid-area: nine"
+  aria-label="9"
+>
+  9
+</button>
+<!-- 8 Button -->
+<button
+  class="calc-button input"
+  data-name="eight"
+  data-value="8"
+  style="grid-area: eight"
+  aria-label="8"
+>
+  8
+</button>
+<!-- 7 Button -->
+<button
+  class="calc-button input"
+  data-name="seven"
+  data-value="7"
+  style="grid-area: seven"
+  aria-label="7"
+>
+  7
+</button>
+<!-- 6 Button -->
+<button
+  class="calc-button input"
+  data-name="six"
+  data-value="6"
+  style="grid-area: six"
+  aria-label="6"
+>
+  6
+</button>
+<!-- 5 Button -->
+<button
+  class="calc-button input"
+  data-name="five"
+  data-value="5"
+  style="grid-area: five"
+  aria-label="5"
+>
+  5
+</button>
+<!-- 4 Button -->
+<button
+  class="calc-button input"
+  data-name="four"
+  data-value="4"
+  style="grid-area: four"
+  aria-label="4"
+>
+  4
+</button>
+<!-- 3 Button -->
+<button
+  class="calc-button input"
+  data-name="three"
+  data-value="3"
+  style="grid-area: three"
+  aria-label="3"
+>
+  3
+</button>
+<!-- 2 Button -->
+<button
+  class="calc-button input"
+  data-name="two"
+  data-value="2"
+  style="grid-area: two"
+  aria-label="2"
+>
+  2
+</button>
+<!-- 1 Button -->
+<button
+  class="calc-button input"
+  data-name="one"
+  data-value="1"
+  style="grid-area: one"
+  aria-label="1"
+>
+  1
+</button>
+<!-- 0 Button -->
+<button
+  class="calc-button input"
+  data-name="zero"
+  data-value="0"
+  style="grid-area: zero"
+  aria-label="0"
+>
+  0
+</button>
+<!-- Decimal Button -->
+<button
+  class="calc-button input"
+  data-name="dot"
+  data-value="."
+  style="grid-area: dot"
+  aria-label="decimal."
+>
+  .
+</button>
+<!-- Divide Button -->
+<button
+  class="calc-button operator"
+  data-name="div"
+  data-value="÷"
+  style="grid-area: div"
+  aria-label="divide"
+>
+  ÷
+</button>
+<!-- Multiply Button -->
+<button
+  class="calc-button operator"
+  data-name="mul"
+  data-value="×"
+  style="grid-area: mul"
+  aria-label="multipy"
+>
+  ×
+</button>
+<!-- Subtract Button -->
+<button
+  class="calc-button operator"
+  data-name="sub"
+  data-value="−"
+  style="grid-area: sub"
+  aria-label="subtract"
+>
+  −
+</button>
+<!-- Add Button -->
+<button
+  class="calc-button operator"
+  data-name="add"
+  data-value="+"
+  style="grid-area: add"
+  aria-label="add"
+>
+  +
+</button>
+<!-- Exponent Button -->
+<button
+  class="calc-button operator"
+  data-name="exp"
+  data-value="^"
+  style="grid-area: exp"
+  aria-label="exponent"
+>
+  <img class="exp" src="./images/exp.svg" alt="exponent symbol."/>
+</button>
+<!-- Backspace Button -->
+<button
+  class="calc-button"
+  data-name="backspace"
+  style="grid-area: backspace"
+  aria-label="backspace"
+>
+  <img class="backspace" src="./images/backspace.svg" alt="back arrow." />
+</button>
+`;
